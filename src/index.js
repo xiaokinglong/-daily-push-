ab73ccd63a17ee230f6c1c31d507f5ccf170dec1
xiaokinglong <xiaokinglong@gmail.com>
2021-10-03 10:19:52 +0800

    13

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { sendMessage } from "./sendWChat/index.js";
 import { config } from "./config.js";
 
 import getFundsData from "./fund.js";
-async  function main() {
+async function main() {
   const info = await getFundsData();
-  sendMessage(config.currentDay,info, config.SCKEY)
+  sendMessage(config.currentDay, info, config.SCKEY);
 }
 
 main();
